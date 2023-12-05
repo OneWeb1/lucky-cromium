40b9b17d16b879716d2903c815fc94b11f67f5d9
OneWeb1 <alicksadler18@gmail.com>
2023-12-05 06:30:26 +0200

    'reload'

--- index.js
+++ index.js
@@ -12,8 +12,13 @@ const url =
 	'https://lucky-jet.gamedev-atech.cc/?exitUrl=null&language=uk&b=8137c4e5b3acab20ae1f3beb43efac9a78fcaa7c4a27cd6e8a02bf5074ba8de9857cf583774d79836cd757f40d50d77dd3b276632df2208273725233bae66c554f83a1d1981b5f2fb0b84ed7222c2f5399a3c25fb5752c3859468772cedb1166b93c6f9070.80429a59887f8724d9270af78d143b0a';
 
 const app = express();
+const corsOptions = {
+	origin: 'https://your-allowed-origin.com',
+	optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cors());
 
 const p = {};
 
