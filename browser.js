81ea18ea9bd695792bd94e3006679a338d22fa65
AlickSadler <alicksadler@gmail.com>
2023-12-12 09:36:14 +0200

    'f'

--- browser.js
+++ browser.js
@@ -22,7 +22,7 @@ const launch = async () => {
 			process.env.NODE_ENV === 'production'
 				? process.env.PUPPETEER_EXECUTABLE_PATH
 				: puppeteer.executablePath(),
-		// headless: chromium.headless,
+		headless: 'new' || chromium.headless,
 		// ignoreHTTPSErrors: false,
 		// protocolTimeout: 1000000,
 	});
